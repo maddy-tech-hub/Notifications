0b9e751c8d597ed1999e5e126849bbd76cafdf30
Madhava Reddy <90255418+Madhava-Reddy@users.noreply.github.com>
2025-02-13 19:20:16 +0530

    Update Program.cs

--- Notifications/Program.cs
+++ Notifications/Program.cs
@@ -29,7 +29,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notifications API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
